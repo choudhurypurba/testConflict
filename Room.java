e4d9ea88f1ec6380f68f335120d55bbd368c681f
choudhurypurba <choudhurypurba@yahoo.com>
2018-12-07 14:14:56 +0100

    alreaybooked

---
+++ Room.java
@@ -0,0 +1,18 @@
+public class Room {
+
+    private int roomNumber;
+    private int numberOfBeds;
+    private boolean hasBalcony;
+    private double pricePerNight;
+    private boolean alreadyBooked;
+
+
+
+    public void Room(int roomNbr, int numberOfBeds, boolean hasBalcony, double pricePerNight) {
+
+    }
+}
+
+
+
+
